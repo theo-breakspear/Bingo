aa2cb8b14d34f279b64a161a1f4aa5e8ecf86d01
theo-breakspear <136743256+theo-breakspear@users.noreply.github.com>
2023-07-21 13:55:58 +0100

    separate part1 input
    - extract numbers as list of strings
    - extract card as string

---
+++ Part1/part1.py
@@ -0,0 +1,12 @@
+# separate numbers and card
+# extract card columns and rows
+# for each number check each row and column and remove number if exists
+# check if any empty rows or columns
+
+input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11 0\n8 2 23 4 24\n21 9 14 16 7\n6 10 3 18 5\n1 12 20 15 19"
+
+numbersString, cardString = input.split("\n\n")
+
+numbers = numbersString.split(",")
+
+print(numbers)
