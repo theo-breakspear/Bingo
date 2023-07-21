838a69beb0dd3b29ce8fb84b28c7c121bb253e78
theo-breakspear <136743256+theo-breakspear@users.noreply.github.com>
2023-07-21 20:09:50 +0100

    create playBingo method on card

--- Card.py
+++ Card.py
@@ -1,10 +1,33 @@
+from typing import List
+
+
 class Card:
-    def __init__(self, card_string):
+    def __init__(self, card_string: str):
         self.rows = [line.split(" ") for line in card_string.split("\n")]
         self.columns = self._initialize_columns(self.rows)
 
-    def _initialize_columns(self, rows):
+    def _initialize_columns(self, rows: List[List[str]]):
         columns = []
         for i in range(len(rows)):
             columns.append([row[i] for row in rows])
         return columns
+
+    def playBingo(self, numbers: List[str]) -> int or None:
+        rows = self.rows
+        columns = self.columns
+        call_count = 0
+        for number in numbers:
+            for row in rows:
+                if number in row:
+                    row.pop(row.index(number))
+            for column in columns:
+                if number in column:
+                    column.pop(column.index(number))
+            call_count += 1
+            for row in rows:
+                if not row:
+                    return call_count
+            for column in columns:
+                if not column:
+                    return call_count
+        return None
--- part1.py
+++ part1.py
@@ -11,8 +11,7 @@ input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\
 numbers_string, card_string = input.split("\n\n")
 
 numbers = numbers_string.split(",")
-
 card = Card(card_string)
 
-print(card.rows)
-print(card.columns)
+win = card.playBingo(numbers)
+print(win)
